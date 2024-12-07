79a4aae32f87863241db76c27288e31fa729b365
carlos velez <gpmotos1@gmail.com>
2024-12-07 13:14:00 -0500

    create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,23 @@
-import './index.css'
+import "./index.css";
 
 export const Footer = () => {
   return (
-    <div className={'footerContainer'}>Footer</div>
-  )
-}
+    <div className={"footerContainer"}>
+      <img src={"src/assets/grafica_portatiles.png"} alt={"logo"} />
+      <div className={"footerContent"}>
+        <span className={"sub-title"}>Conclusiones</span>
+
+        <p>
+          La ejecución del código ha permitido la extracción efectiva de datos
+          sobre computadoras portátiles desde la plataforma de Mercado Libre. Los
+          resultados incluyen un conjunto de marcas y precios, lo que proporciona
+          una visión general del mercado disponible. La estructura del código
+          facilita la navegación a través de las páginas, asegurando que se
+          recojan múltiples entradas de productos, y el manejo de excepciones ha
+          permitido identificar elementos problemáticos.
+        </p>
+
+      </div>
+    </div>
+  );
+};
