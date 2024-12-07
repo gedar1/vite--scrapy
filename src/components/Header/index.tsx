bf388a1667367522bd9136e9bec63de55e8b5144
jcpresiga <jcpresiga@gmail.com>
2024-12-07 12:58:17 -0500

    update header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,29 @@ import './index.css'
 
 export const Header = () => {
   return (
-    <div className={'headerContainer'}>Header is test Two</div>
+    <div className={"headerContainer"}>
+      <span className={'title'}>Análisis y herramientas de extracción de datos</span>
+    <div className={"headerContent"}>
+      <div className={"headerContentInner"}>
+        <div className="headerContentInnerItems">
+      <span className={"sub-title"}>Estudiantes</span>
+          <span>German Darío Arbeláez Ríos</span>
+          <span> Juan Carlos Presiga Montoya</span>
+          <span>Carlos Alberto Velez Muñoz</span>
+        </div>
+        <div>
+          <span className={"icon-uni"}>
+            <img src="src/assets/iud_logo.png" alt="icon-uni" />
+          </span>
+        </div>
+        <div className="headerContentInnerGroup">
+          <span>Grupo #7</span>
+          <span>Grupo 2402B020107</span>
+        </div>
+      </div>
+    </div>
+    </div>
+
   )
 }
 
